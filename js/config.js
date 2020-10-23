ceb92d993d58a1075653aff88e65574c527e5d62
boyetp <boyet.palmer@gmail.com>
2020-10-23 11:15:50 -0400

    Cognito Update.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_48Kbo8Fk8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '27t5hi6dvb8964b6juo45v05ma', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
